3957a3e0b18c0bba44e85ce71ee9ed52fa3f506d
llayz46 <layzer@outlook.fr>
2024-02-08 15:25:33 +0100

    ?

---
+++ scripts/accordion.js
@@ -0,0 +1,18 @@
+const accordionContent = document.getElementsByClassName('js-accordion-content')
+const accordionFaq = document.getElementsByClassName('js-accordion-faq')
+
+const accordion = (accordion) => {
+  for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function() {
+      this.classList.toggle('active')
+    })
+  }
+}
+
+accordion(accordionContent)
+accordion(accordionFaq)
+
+const okay = document.querySelector('.test')
+okay.addEventListener('click', () => {
+  okay.classList.toggle('active')
+})
